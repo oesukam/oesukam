31a18f620f738367e01fbeb8b52b2cde59a00b29
Olivier ESUKA <oesukam@gmail.com>
2018-09-17 11:03:53 +0200

    UCK university added

--- pages/_lang/teaching.vue
+++ pages/_lang/teaching.vue
@@ -9,6 +9,17 @@
               <h3 class="title is-3 year-title">2018</h3>
               <h4 class="title is-3 acad-title">{{$t('teaching.academic_year')}}</h4>
             </div>
+            <!-- UCK -->
+            <div class="timeline">
+              <div class="timeline-item">
+                <h4 class="item-title">UCK - Université Catholique de Kisangani</h4>
+                <span class="item-small">Kisangani</span>
+                <!-- <ul class="item-list">
+                  <li>Système d’informatique et les bases de données</li>
+                </ul> -->
+              </div>
+            </div>
+
             <!-- UMC -->
             <div class="timeline">
               <div class="timeline-item">
